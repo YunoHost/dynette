46736549d33893ccf8419bc9f7dab8002430b12b
Kload <kload@kload.fr>
2013-06-15 19:57:12 +0000

    Add pattern matching to url params

--- dynette.rb
+++ dynette.rb
@@ -28,18 +28,37 @@ class Ip
     belongs_to :entry
 end
 
-#get '/' do
-    #`whoami`
-#end
+get '/' do
+    `whoami`
+end
 
 post '/' do
     content_type :json
-    # TODO: check params
+    # Check params
+    unless params[:subdomain].match /^[a-z0-9-]{3,16}$/
+        status 400
+        return { :error => "Subdomain is invalid: #{params[:subdomain]}.#{DOMAIN}" }
+    end
+    unless params[:public_key].match /^[a-z0-9]{22}==$/i
+        status 400
+        return { :error => "Key is invalid: #{params[:public_key]}" }
+    end
+    unless params[:current_ip].match /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+        status 400
+        return { :error => "Key is invalid: #{params[:current_ip]}" }
+    end
+
+    # If already exists
+    if entry = Entry.first(:subdomain => params[:subdomain])
+        status 409
+        return { :error => "Subdomain already taken: #{entry.subdomain}.#{DOMAIN}" }
+    end
     if entry = Entry.first(:public_key => params[:public_key])
         status 409
         return { :error => "Key already exists for domain #{entry.subdomain}.#{DOMAIN}" }
     end
     entry = Entry.new(:public_key => params[:public_key], :subdomain => params[:subdomain], :current_ip => request.ip)
+    entry.ips << Ip.create(:ip_addr => request.ip)
     if entry.save
         status 201
         return { :public_key => params[:public_key], :subdomain => params[:subdomain], :current_ip => request.ip }.to_json
@@ -57,7 +76,5 @@ get '/all' do
     content_type :json
     Entry.all.to_json
 end
-
-
 
 DataMapper.auto_upgrade!
