e1f16c3722118560a9c0ff200628319294d89786
Kload <kload@kload.fr>
2013-07-07 11:17:58 +0000

    base64 dns key

--- dynette.rb
+++ dynette.rb
@@ -72,6 +72,7 @@ end
     before path do
         if params.has_key?("public_key")
             public_key = Base64.decode64(params[:public_key])
+            puts public_key
             unless public_key.length == 24
                 halt 400, { :error => "Key is invalid: #{public_key.to_s.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => '?'})}" }.to_json
             end
