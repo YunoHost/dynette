4688baa0ca13759219f656fcd42da419b77645b6
Félix Piédallu <felix@piedallu.me>
2026-03-07 14:58:16 +0100

    Refactorize, create a dynette class that handles the stuff behind the http proxy

--- src/dynette/app.py
+++ src/dynette/app.py
@@ -1,230 +1,142 @@
 #!/usr/bin/env python3
+from pathlib import Path
 
-import hmac
-import base64
-import os
-import re
 import yaml
-import bcrypt
-
 from flask import Flask, jsonify, request
+from flask.typing import ResponseReturnValue
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-DOMAIN_REGEX = re.compile(
-    r"^([a-z0-9]{1}([a-z0-9\-]*[a-z0-9])*)(\.[a-z0-9]{1}([a-z0-9\-]*[a-z0-9])*)*(\.[a-z]{1}([a-z0-9\-]*[a-z0-9])*)$"
-)
+from .dynette import Dynette, ForbiddenError
 
-def trusted_ip():
+CONFIG_FILE = Path.cwd() / "config.yml"
+
+
+def trusted_ip() -> bool:
     # This is for example the CI, or developers testing new developments
-    if request.remote_addr in app.config.get("LIMIT_EXEMPTED_IPS", []):
-        return True
-    if request.environ.get("HTTP_X_FORWARDED_HOST") in app.config.get("LIMIT_EXEMPTED_IPS", []):
-        return True
-    return False
+    exempted_ips: list[str] = app.config.get("LIMIT_EXEMPTED_IPS", [])
+    ips = (request.remote_addr, request.environ.get("HTTP_X_FORWARDED_HOST"))
+    return any(ip in exempted_ips for ip in ips)
+
 
 app = Flask(__name__)
-app.config.from_file(os.path.join(os.getcwd(), "config.yml"), load=yaml.safe_load)
+app.config.from_file(str(CONFIG_FILE), load=yaml.safe_load)
 # cf. https://flask-limiter.readthedocs.io/en/stable/recipes.html#deploying-an-application-behind-a-proxy
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 limiter = Limiter(
     get_remote_address,
     app=app,
     default_limits=["50 per hour"],
-    #storage_uri="memory://",   # <- For development
+    # storage_uri="memory://",   # <- For development
     storage_uri="redis://localhost:6379",
     storage_options={"socket_connect_timeout": 30},
-    strategy="fixed-window", # or "moving-window"
+    strategy="fixed-window",  # or "moving-window"
     application_limits_exempt_when=trusted_ip,
     default_limits_exempt_when=trusted_ip,
 )
 
-assert os.path.isdir(
-    app.config["DB_FOLDER"]
-), "You should create the DB folder declared in the config"
+DB_FOLDER = Path(app.config["DB_FOLDER"]).resolve()
+assert DB_FOLDER.is_dir(), "You should create the DB folder declared in the config"
 
-
-def _validate_domain(domain):
-
-    if not DOMAIN_REGEX.match(domain):
-        return {"error": f"This is not a valid domain: {domain}"}, 400
-
-    if (
-        len(domain.split(".")) != 3
-        or domain.split(".", 1)[-1] not in app.config["DOMAINS"]
-    ):
-        return {"error": "This subdomain is not handled by this dynette server."}, 400
-
-
-def _is_available(domain):
-
-    return not os.path.exists(f"{app.config['DB_FOLDER']}/{domain}.key")
-
+DYNETTE = Dynette(DB_FOLDER, app.config["DOMAINS"])
 
 @app.route("/")
 @limiter.exempt
-def home():
+def home() -> ResponseReturnValue:
     return "Wanna play the dynette?"
 
 
 @app.route("/domains")
 @limiter.exempt
-def domains():
+def domains() -> ResponseReturnValue:
     return jsonify(app.config["DOMAINS"]), 200
 
 
-@app.route("/test/<domain>")
+@app.route("/test/<string:domain>")
 @limiter.limit("5 per hour", exempt_when=trusted_ip)
-def availability(domain):
+def availability(domain: str) -> ResponseReturnValue:
+    try:
+        DYNETTE.validate(domain)
+    except (TypeError, ValueError) as err:
+        return {"error": str(err)}, 400
 
-    error = _validate_domain(domain)
-    if error:
-        return error
-
-    if _is_available(domain):
-        return f'"Domain {domain} is available"', 200
-    else:
+    if not DYNETTE.available(domain):
         return {"error": f"Subdomain already taken: {domain}"}, 409
 
+    return f'"Domain {domain} is available"', 200
 
-@app.route("/key/<key>", methods=["POST"])
+
+@app.route("/key/<string:key>", methods=["POST"])
 @limiter.limit("5 per hour", exempt_when=trusted_ip)
-def register(key):
-
+def register(key: str) -> ResponseReturnValue:
     try:
-        key = base64.b64decode(key).decode()
-    except Exception as e:
-        return {"error": "Key format is invalid"}, 400
-    else:
-        if len(key) != 89:
-            return {"error": "Key format is invalid"}, 400
-
-    try:
-        data = dict(request.form)  # get_json(force=True)
-        subdomain = data.get("subdomain")
+        subdomain = request.form.get("subdomain")
         assert isinstance(subdomain, str)
-    except Exception as e:
-        return {"error": f"Invalid request: {str(request.form)}"}, 400
+        recovery_password = request.form.get("recovery_password")
+    except (ValueError, AssertionError):
+        return {"error": f"Invalid request: {request.form!r}"}, 400
 
-    error = _validate_domain(subdomain)
-    if error:
-        return error
+    try:
+        DYNETTE.validate(subdomain)
+        if not DYNETTE.available(subdomain):
+            return {"error": f"Subdomain already taken: {subdomain}"}, 409
+        DYNETTE.register(subdomain, key, recovery_password)
 
-    if not _is_available(subdomain):
-        return {"error": f"Subdomain already taken: {subdomain}"}, 409
-
-    recovery_password = data.get("recovery_password")
-    if recovery_password and isinstance(recovery_password, str):
-        if len(recovery_password) < 8:
-            return {"error": "Recovery password too short"}, 409
-        if len(recovery_password) > 1024:
-            return {"error": "Recovery password too long"}, 409
-
-        recovery_password = bcrypt.hashpw(
-            password=recovery_password.encode(), salt=bcrypt.gensalt(14)
-        )
-        recovery_password = base64.b64encode(recovery_password).decode()
-
-    with open(f"{app.config['DB_FOLDER']}/{subdomain}.key", "w") as f:
-        f.write(key)
-
-    if recovery_password:
-        with open(f"{app.config['DB_FOLDER']}/{subdomain}.recovery_password", "w") as f:
-            f.write(recovery_password)
+    except (TypeError, ValueError, AssertionError) as err:
+        return {"error": str(err)}, 400
+    except ForbiddenError:
+        return '"Access Denied"', 403
 
     return '"OK"', 201
 
 
-@app.route("/domains/<subdomain>", methods=["DELETE"])
+@app.route("/domains/<string:subdomain>", methods=["DELETE"])
 @limiter.limit("5 per hour", exempt_when=trusted_ip)
-def delete_using_recovery_password_or_key(subdomain):
-
+def delete_using_recovery_password_or_key(subdomain: str) -> ResponseReturnValue:
     try:
-        assert isinstance(subdomain, str)
-        data = dict(request.form)  # get_json(force=True)
-        recovery_password = data.get("recovery_password")
-        key = data.get("key")
-        assert (recovery_password and isinstance(recovery_password, str)) or (
-            key and isinstance(key, str)
+        recovery_password = request.form.get("recovery_password")
+        key = request.form.get("key")
+        assert any(
+            string and isinstance(string, str) for string in (key, recovery_password)
         )
-        if key:
-            key = base64.b64decode(key).decode()
-    except Exception:
+    except (ValueError, AssertionError):
         return {"error": "Invalid request"}, 400
 
-    error = _validate_domain(subdomain)
-    if error:
-        return error
+    try:
+        DYNETTE.validate(subdomain)
+        if DYNETTE.available(subdomain):
+            return {"error": "Subdomain already deleted"}, 409
+        DYNETTE.delete(subdomain, key, recovery_password)
 
-    if _is_available(subdomain):
-        return {"error": "Subdomain already deleted"}, 409
-
-    if key:
-        with open(f"{app.config['DB_FOLDER']}/{subdomain}.key") as f:
-            if not hmac.compare_digest(key, f.read()):
-                return '"Access denied"', 403
-    elif recovery_password:
-        if not os.path.exists(
-            f"{app.config['DB_FOLDER']}/{subdomain}.recovery_password"
-        ):
-            return '"Access denied"', 403
-        with open(f"{app.config['DB_FOLDER']}/{subdomain}.recovery_password") as f:
-            hashed = base64.b64decode(f.read())
-
-        if not bcrypt.checkpw(recovery_password.encode(), hashed):
-            return '"Access denied"', 403
-    # Shouldnt happen, this is checked before
-    else:
-        return '"Access denied"', 403
-
-    if os.path.exists(f"{app.config['DB_FOLDER']}/{subdomain}.key"):
-        os.remove(f"{app.config['DB_FOLDER']}/{subdomain}.key")
-    if os.path.exists(f"{app.config['DB_FOLDER']}/{subdomain}.recovery_password"):
-        os.remove(f"{app.config['DB_FOLDER']}/{subdomain}.recovery_password")
+    except (TypeError, ValueError, AssertionError) as err:
+        return {"error": str(err)}, 400
+    except ForbiddenError:
+        return '"Access Denied"', 403
 
     return '"OK"', 200
 
 
-@app.route("/domains/<subdomain>/recovery_password", methods=["PUT"])
+@app.route("/domains/<string:subdomain>/recovery_password", methods=["PUT"])
 @limiter.limit("5 per hour", exempt_when=trusted_ip)
-def set_recovery_password_using_key(subdomain):
-
+def set_recovery_password_using_key(subdomain: str) -> ResponseReturnValue:
     try:
-        assert isinstance(subdomain, str)
-        data = dict(request.form)  # get_json(force=True)
-        recovery_password = data.get("recovery_password")
-        key = data.get("key")
-        assert (recovery_password and isinstance(recovery_password, str)) and (
-            key and isinstance(key, str)
-        )
-        if key:
-            key = base64.b64decode(key).decode()
-    except Exception:
+        recovery_password = request.form.get("recovery_password")
+        key = request.form.get("key")
+        assert recovery_password and isinstance(recovery_password, str)
+        assert key and isinstance(key, str)
+    except (ValueError, AssertionError):
         return {"error": "Invalid request"}, 400
 
-    error = _validate_domain(subdomain)
-    if error:
-        return error
+    try:
+        DYNETTE.validate(subdomain)
+        if DYNETTE.available(subdomain):
+            return {"error": "Subdomain not registered"}, 404
+        DYNETTE.set_password(subdomain, key, recovery_password)
 
-    if _is_available(subdomain):
-        return {"error": "Subdomain not registered"}, 404
-
-    with open(f"{app.config['DB_FOLDER']}/{subdomain}.key") as f:
-        if not hmac.compare_digest(key, f.read()):
-            return '"Access denied"', 403
-
-    if len(recovery_password) < 8:
-        return {"error": "Recovery password too short"}, 409
-    if len(recovery_password) > 1024:
-        return {"error": "Recovery password too long"}, 409
-
-    recovery_password = bcrypt.hashpw(
-        password=recovery_password.encode(), salt=bcrypt.gensalt(14)
-    )
-    recovery_password = base64.b64encode(recovery_password).decode()
-
-    with open(f"{app.config['DB_FOLDER']}/{subdomain}.recovery_password", "w") as f:
-        f.write(recovery_password)
+    except (TypeError, ValueError, AssertionError) as err:
+        return {"error": str(err)}, 400
+    except ForbiddenError:
+        return '"Access Denied"', 403
 
     return '"OK"', 200
--- src/dynette/regen_named_conf.py
+++ src/dynette/regen_named_conf.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python3
 
-import os
-import yaml
-import glob
-import jinja2
+import subprocess
+from pathlib import Path
 
-DYNETTE_DIR = os.path.dirname(__file__)
-TEMPLATES_DIR = os.path.join(DYNETTE_DIR, "templates")
+import jinja2
+import yaml
+
+DYNETTE_DIR = Path(__file__).resolve().parent
+TEMPLATES_DIR = DYNETTE_DIR / "templates"
+
+CONFIG_FILE = Path("./config.yml")
 
 
 def main() -> None:
-    config = yaml.safe_load(open("config.yml").read())
+    config = yaml.safe_load(CONFIG_FILE.open())
+    db_folder = Path(config["DB_FOLDER"])
 
     domains = [{"name": domain, "subdomains": []} for domain in config["DOMAINS"]]
 
     for infos in domains:
         domain = infos["name"]
-        for f in glob.glob(config["DB_FOLDER"] + f"*.{domain}.key"):
-            key = open(f).read()
-            subdomain = f.split("/")[-1].rsplit(".", 1)[0]
+        for file in db_folder.glob(f"*.{domain}.key"):
+            subdomain = file.name.rsplit(".", 1)[0]
+            key = file.read_text().strip()
             infos["subdomains"].append({"name": subdomain, "key": key})
 
-    templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATES_DIR)
-    templateEnv = jinja2.Environment(loader=templateLoader)
-    template = templateEnv.get_template("named.conf.j2")
-    named_conf = template.render(domains=domains)
+    template_loader = jinja2.FileSystemLoader(searchpath=TEMPLATES_DIR)
+    template_environ = jinja2.Environment(loader=template_loader)
+    template = template_environ.get_template("named.conf.j2")
 
-    open('/etc/bind/named.conf.local', 'w').write(named_conf)
-    os.system('chown -R bind:bind /etc/bind/named.conf.local /var/lib/bind/')
-    os.system('/usr/sbin/rndc reload')
+    named_conf = template.render(domains=domains)
+    Path("/etc/bind/named.conf.local").write_text(named_conf)
+
+    subprocess.check_call(
+        ["chown", "-R", "bind:bind", "/etc/bind/named.conf.local", "/var/lib/bind/"]
+    )
+    subprocess.check_call(["/usr/sbin/rndc", "reload"])
 
 
 if __name__ == "__main__":
