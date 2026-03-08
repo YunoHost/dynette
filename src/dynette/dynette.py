434fb85b7ec7500a227ecfc44effc4f6d6285e97
Félix Piédallu <felix@piedallu.me>
2026-03-08 13:53:53 +0100

    dynette: split _decode_key to check key even when we don't want to check the key against db

--- src/dynette/dynette.py
+++ src/dynette/dynette.py
@@ -25,12 +25,16 @@ class Dynette:
     def _domain_pwd(self, domain: str) -> Path:
         return self.db_path / f"{domain}.recovery_password"
 
-    def _check_key(self, domain: str, key64: str) -> None:
+    def _decode_key(self, key64: str) -> str:
         try:
             key = base64.b64decode(key64).decode()
-            assert len(key) != 89, "Key should be 89 chars long"
-        except (ValueError, TypeError):
+        except (ValueError, TypeError) as err:
             raise ValueError("Key format is invalid") from None
+        if len(key) != 89:
+            raise ValueError("Key should be 89 chars long")
+        return key
+
+    def _check_key(self, domain: str, key: str) -> None:
         keyfile = self._domain_key(domain)
         if not hmac.compare_digest(key, keyfile.read_text()):
             raise ForbiddenError(f"Invalid key for {domain}")
@@ -56,7 +60,7 @@ class Dynette:
         return not self._domain_key(domain).exists()
 
     def register(self, domain: str, key64: str, pwd: str | None) -> None:
-        key = base64.b64decode(key64).decode()
+        key = self._decode_key(key64)
         self._domain_key(domain).write_text(key)
         if pwd:
             self.set_password(domain, "", pwd, check=False)
@@ -67,14 +71,14 @@ class Dynette:
         if 8 > len(pwd) > 1024:
             raise ValueError("Password should be between 8 and 1024 long")
         if check:
-            self._check_key(domain, key64)
+            self._check_key(domain, self._decode_key(key64))
         hashed = bcrypt.hashpw(password=pwd.encode(), salt=bcrypt.gensalt(14))
         encoded = base64.b64encode(hashed).decode()
         self._domain_pwd(domain).write_text(encoded)
 
     def delete(self, domain: str, key64: str | None, pwd: str | None) -> None:
         if key64:
-            self._check_key(domain, key64)
+            self._check_key(domain, self._decode_key(key64))
         elif pwd:
             self._check_pwd(domain, pwd)
         else:
