4e592668710988eca2e1e046e8c41aeb469842d8
Félix Piédallu <felix@piedallu.me>
2026-03-08 13:54:52 +0100

    app: Simplify code, avoid using asserts

--- src/dynette/app.py
+++ src/dynette/app.py
@@ -72,58 +72,48 @@ def create_app(test_config: dict[str, Any] | None = None) -> Flask:
 
         return f'"Domain {domain} is available"', 200
 
-    @app.route("/domains/<string:subdomain>", methods=["POST"])
-    @limiter.limit("5 per hour", exempt_when=trusted_ip)
-    def register(subdomain: str) -> ResponseReturnValue:
-        try:
-            data = dict(request.form)
-            recovery_password = request.form.get("recovery_password")
-            key = request.form.get("key")
-            assert isinstance(key, str)
-            recovery_password = request.form.get("recovery_password")
-        except (ValueError, AssertionError):
-            return {"error": f"Invalid request: {data}"}, 400
+    def _register(
+        subdomain: str | None, key: str | None, pwd: str | None
+    ) -> ResponseReturnValue:
+        if not (
+            isinstance(subdomain, str)
+            and isinstance(key, str)
+            and isinstance(pwd, str | None)
+        ):
+            return {"error": f"Invalid request: {dict(request.form)}"}, 400
 
         try:
             dynette.validate(subdomain)
             if not dynette.available(subdomain):
                 return {"error": f"domain already taken: {subdomain}"}, 409
-            dynette.register(subdomain, key, recovery_password)
-
-        except (TypeError, ValueError, AssertionError) as err:
+            dynette.register(subdomain, key, pwd)
+        except (TypeError, ValueError) as err:
             return {"error": str(err)}, 400
         except ForbiddenError:
             return '"Access Denied"', 403
-
         return '"OK"', 201
+
+    @app.route("/domains/<string:subdomain>", methods=["POST"])
+    @limiter.limit("5 per hour", exempt_when=trusted_ip)
+    def register(subdomain: str) -> ResponseReturnValue:
+        key = request.form.get("key")
+        recovery_password = request.form.get("recovery_password")
+        return _register(subdomain, key, recovery_password)
 
     @app.route("/key/<string:key>", methods=["POST"])
     @limiter.limit("5 per hour", exempt_when=trusted_ip)
     def register_via_key(key: str) -> ResponseReturnValue:
-        try:
-            subdomain = request.form.get("subdomain")
-            assert isinstance(subdomain, str)
-        except (ValueError, AssertionError):
-            return {"error": f"Invalid request: {request.form!r}"}, 400
-
-        if (pwd := request.form.get("recovery_password")) is not None:
-            request.form["recovery_password"] = pwd
-        request.form["recovery_password"] = key
-
-        return register(subdomain)
+        subdomain = request.form.get("subdomain")
+        recovery_password = request.form.get("recovery_password")
+        return _register(subdomain, key, recovery_password)
 
     @app.route("/domains/<string:subdomain>", methods=["DELETE"])
     @limiter.limit("5 per hour", exempt_when=trusted_ip)
     def delete_using_recovery_password_or_key(subdomain: str) -> ResponseReturnValue:
-        try:
-            recovery_password = request.form.get("recovery_password")
-            key = request.form.get("key")
-            assert any(
-                string and isinstance(string, str)
-                for string in (key, recovery_password)
-            )
-        except (ValueError, AssertionError):
-            return {"error": "Invalid request"}, 400
+        key = request.form.get("key")
+        recovery_password = request.form.get("recovery_password")
+        if not (isinstance(key, str) or isinstance(recovery_password, str)):
+            return {"error": f"Invalid request: {dict(request.form)}"}, 400
 
         try:
             dynette.validate(subdomain)
@@ -131,7 +121,7 @@ def create_app(test_config: dict[str, Any] | None = None) -> Flask:
                 return {"error": "Subdomain already deleted"}, 409
             dynette.delete(subdomain, key, recovery_password)
 
-        except (TypeError, ValueError, AssertionError) as err:
+        except (TypeError, ValueError) as err:
             return {"error": str(err)}, 400
         except ForbiddenError:
             return '"Access Denied"', 403
@@ -141,13 +131,10 @@ def create_app(test_config: dict[str, Any] | None = None) -> Flask:
     @app.route("/domains/<string:subdomain>/recovery_password", methods=["PUT"])
     @limiter.limit("5 per hour", exempt_when=trusted_ip)
     def set_recovery_password_using_key(subdomain: str) -> ResponseReturnValue:
-        try:
-            recovery_password = request.form.get("recovery_password")
-            key = request.form.get("key")
-            assert recovery_password and isinstance(recovery_password, str)
-            assert key and isinstance(key, str)
-        except (ValueError, AssertionError):
-            return {"error": "Invalid request"}, 400
+        key = request.form.get("key")
+        recovery_password = request.form.get("recovery_password")
+        if not (isinstance(key, str) and isinstance(recovery_password, str)):
+            return {"error": f"Invalid request: {dict(request.form)}"}, 400
 
         try:
             dynette.validate(subdomain)
@@ -155,7 +142,7 @@ def create_app(test_config: dict[str, Any] | None = None) -> Flask:
                 return {"error": "Subdomain not registered"}, 404
             dynette.set_password(subdomain, key, recovery_password)
 
-        except (TypeError, ValueError, AssertionError) as err:
+        except (TypeError, ValueError) as err:
             return {"error": str(err)}, 400
         except ForbiddenError:
             return '"Access Denied"', 403
